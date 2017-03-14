f7582a6393716c5fc36e39969582485c39c54b3b
João Rangel <joaohcrangel@gmail.com>
2017-03-14 16:29:50 -0300

    inicio do projeto

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php 
+
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+    
+	echo "OK";
+
+});
+
+$app->run();
+
+ ?>

88c0adb98797e2a5905206f3237cdfb4af15e199
João Rangel <joaohcrangel@gmail.com>
2017-08-23 11:42:24 -0300

    ok

--- vendor/hcodebr/php-classes/src/DB/Sql.php
+++ vendor/hcodebr/php-classes/src/DB/Sql.php
@@ -5,8 +5,8 @@ namespace Hcode\DB;
 class Sql {
 
 	const HOSTNAME = "127.0.0.1";
-	const USERNAME = "root";
-	const PASSWORD = "root";
+	const USERNAME = "hcode";
+	const PASSWORD = "ZAQ!xsw2";
 	const DBNAME = "db_ecommerce";
 
 	private $conn;

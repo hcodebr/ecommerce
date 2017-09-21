3bd79123f1eb79da7ecff29c28729788eaaf2830
João Rangel <joaohcrangel@gmail.com>
2017-09-21 12:55:42 -0300

    Admin - Usuarios - Alterar Senha e Excluir Usuarios

--- admin-users.php
+++ admin-users.php
@@ -3,6 +3,64 @@
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
 
+$app->get("/admin/users/:iduser/password", function($iduser){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-password", [
+		'msgSuccess'=>User::getSuccess(),
+		'msgError'=>User::getError(),
+		'user'=>$user->getValues()
+	]);
+
+});
+
+$app->post("/admin/users/:iduser/password", function($iduser){
+
+	User::verifyLogin();
+
+	if (isset($_POST['despassword']) && $_POST['despassword']===''){
+
+		User::setError("Preencha a nova senha.");
+		header("Location: /admin/users/$iduser/password");
+		exit;
+
+	}
+
+	if (isset($_POST['despassword-confirm']) && $_POST['despassword-confirm']===''){
+
+		User::setError("Preencha a confirmação da nova senha.");
+		header("Location: /admin/users/$iduser/password");
+		exit;
+
+	}
+
+	if ($_POST['despassword'] !== $_POST['despassword-confirm']){
+
+		User::setError("Confirme a nova senha corretamente.");
+		header("Location: /admin/users/$iduser/password");
+		exit;
+
+	}
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->setPassword(User::getPasswordHash($_POST['despassword']));
+
+	User::setSuccess("Senha alterada com sucesso.");
+	header("Location: /admin/users/$iduser/password");
+	exit;
+
+});
+
 $app->get("/admin/users", function() {
 
 	User::verifyLogin();

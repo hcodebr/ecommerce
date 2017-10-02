971fbe8f7ffc1ed7049479aef30c33c724ea97b4
João Rangel <joaohcrangel@gmail.com>
2017-10-02 16:37:11 -0300

    Integração PayPal HTML

--- site.php
+++ site.php
@@ -253,7 +253,18 @@ $app->post("/checkout", function(){
 
 	$order->save();
 
-	header("Location: /order/".$order->getidorder()."/pagseguro");
+	switch ((int)$_POST['payment-method']) {
+
+		case 1:
+		header("Location: /order/".$order->getidorder()."/pagseguro");
+		break;
+
+		case 2:
+		header("Location: /order/".$order->getidorder()."/paypal");
+		break;
+
+	}
+
 	exit;
 
 });
@@ -281,6 +292,30 @@ $app->get("/order/:idorder/pagseguro", function($idorder){
 			'areaCode'=>substr($order->getnrphone(), 0, 2),
 			'number'=>substr($order->getnrphone(), 2, strlen($order->getnrphone()))
 		]
+	]);
+
+
+});
+
+$app->get("/order/:idorder/paypal", function($idorder){
+
+	User::verifyLogin(false);
+
+	$order = new Order();
+
+	$order->get((int)$idorder);
+
+	$cart = $order->getCart();
+
+	$page = new Page([
+		'header'=>false,
+		'footer'=>false
+	]);
+
+	$page->setTpl("payment-paypal", [
+		'order'=>$order->getValues(),
+		'cart'=>$cart->getValues(),
+		'products'=>$cart->getProducts()
 	]);
 
 
--- views-cache/checkout.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/checkout.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -113,6 +113,14 @@
 													</tr>
 												</tfoot>
 											</table>
+											<p id="billing_state_field" class="form-row form-row-first address-field validate-state" data-o_class="form-row form-row-first address-field validate-state">
+												<input type="radio" id="method-pagseguro" name="payment-method" placeholder="País" value="1" style="float:left; margin: 30px;">
+												<label class="" for="method-pagseguro"><img style="height:64px;" src="/res/site/img/logo-pagseguro.png"></label>
+											</p>
+											<p id="billing_state_field" class="form-row form-row-first address-field validate-state" data-o_class="form-row form-row-first address-field validate-state">
+												<input type="radio" checked="checked" id="method-paypal" name="payment-method" placeholder="País" value="2" style="float:left; margin: 30px;">
+												<label class="" for="method-paypal"><img style="height:64px;" src="/res/site/img/logo-paypal.png"></label>
+											</p>
 											<div id="payment">
 												<div class="form-row place-order">
 													<input type="submit" data-value="Place order" value="Continuar" id="place_order" name="woocommerce_checkout_place_order" class="button alt">
